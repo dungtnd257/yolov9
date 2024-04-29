b1f277fd9b89f7962137c85779d448a4fd6e3214
dungtnd257 <168316312+dungtnd257@users.noreply.github.com>
2024-04-29 13:55:22 +0700

    Update metrics.py

--- utils/metrics.py
+++ utils/metrics.py
@@ -314,6 +314,7 @@ def bbox_alpha_iou(box1, box2, xywh=True, GIoU=False, DIoU=False, CIoU=False, MD
     inter = (torch.min(b1_x2, b2_x2) - torch.max(b1_x1, b2_x1)).clamp(0) * \
             (torch.min(b1_y2, b2_y2) - torch.max(b1_y1, b2_y1)).clamp(0)
 
+
     # Union Area
     union = w1 * h1 + w2 * h2 - inter + eps
 
