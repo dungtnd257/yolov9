372335fb152c8717b634f26559589e4cacb038a2
dungtnd257 <168316312+dungtnd257@users.noreply.github.com>
2024-04-30 22:01:43 +0700

    Update loss_tal_dual.py

--- utils/loss_tal_dual.py
+++ utils/loss_tal_dual.py
@@ -73,7 +73,7 @@ class BboxLoss(nn.Module):
         bbox_weight = torch.masked_select(target_scores.sum(-1), fg_mask).unsqueeze(-1)
 
         #n_iou
-        iou = bbox_alpha_iou(pred_bboxes_pos, target_bboxes_pos, xywh=False, n=9, CIoU=True)
+        iou = bbox_n_iou(pred_bboxes_pos, target_bboxes_pos, xywh=False, n=9, CIoU=True)
         #alpha iou
         #iou = bbox_alpha_iou(pred_bboxes_pos, target_bboxes_pos, xywh=False, alpha=3, CIoU=True) 
         #iou
